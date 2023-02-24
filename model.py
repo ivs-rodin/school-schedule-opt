c2b4b209cfeccc53652e959e4a36075f8dc0279c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-02-24 22:39:40 +0000

    Deploy ivs-rodin/school-schedule-opt to github.com/ivs-rodin/school-schedule-opt.git:gh-pages

--- model.py
+++ model.py
@@ -5,15 +5,16 @@ class SchoolScheduleOptModel:
     class Subject:
         def __init__(self, subject_name):
             self.name = subject_name
+        #TODO: Compare subjects
 
     class Course:
         def __init__(self, subject, days):
-            self.subject = subject
+            self.subject = Subject(subject)
             self.days = days
 
     class Program:
         def __init__(self, courses):
-            self.courses = courses
+            self.courses = {Course(c['subject'], c['days']) for c in courses}
 
     class Group:
         def __init__(self, group_name, students):
@@ -21,23 +22,23 @@ class SchoolScheduleOptModel:
             self.students = students
 
     class Student:
-        def __init__(self, student_name, student_surname, student_program):
+        def __init__(self, student_name, student_surname, student_courses):
             self.name = student_name
             self.surname = student_surname
-            self.program = student_program
+            self.program = Program(student_courses)
 
     class Teacher:
         def __init__(self, teacher_name, teacher_surname, teacher_subjects, teacher_days):
             self.name = teacher_name
             self.surname = teacher_surname
-            self.subjects = teacher_subjects
+            self.subjects = {Subject(s) for s in teacher_subjects}
             self.days = teacher_days
 
     class Room:
         def __init__(self, room_name, room_capacity, room_subjects):
             self.name = room_name
             self.capacity = room_capacity
-            self.subjects = room_subjects
+            self.subjects = {Subject(s) for s in room_subjects}
         
     class Date:
         def __init__(self, date_dt):
@@ -67,9 +68,17 @@ class SchoolScheduleOptModel:
         # TODO: Read data from database
         pass
 
-    def fill_model(self, data):
+    def fill_model(self):
         # TODO: Fill model with data
 
-        model = ConcreteModel()
+        self.setStudents = {Student(s['name'], s['surname'], s['courses']) for s in self.data['students'].values()}
+        self.setTeachers = {Teacher(t['name'], t['surname'], t['subjects'], t['days']) for t in self.data['teachers'].values()}
+        self.setClassrooms = {Classroom(r['name'], r['capacity'], r['subjects']) for r in self.data['rooms']}
+        self.setSubjects = self.get_subjects()
+
+        self.model = ConcreteModel()
 
         pass
+
+    def get_subjects(self):
+        return {s for subjects in self.setStudents.program.subjects for s in subjects}
